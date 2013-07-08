7174dd3e8cdeee7527ad00fc20a27a0517e617a5
Nikos M <Nikos@Nikos-PC.(none)>
2013-07-08 14:06:13 +0300

    remove obsolete java tool

--- cascades/haartophpj.bat
+++
@@ -1,2 +0,0 @@
-REM %1 is opencv haar xml file name without xml extension
-java -jar haartophp.jar "%1.xml" "%1" > "%1.php"
--- cascades/haartophpj.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# $1 is opencv haar xml file name without xml extension
-java -jar haartophp.jar "$1.xml" "$1" > "$1.php"
